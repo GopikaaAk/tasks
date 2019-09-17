f2563559581004476d169d1e284caeb0b2f7fb2c
GopikaaAk <55086674+GopikaaAk@users.noreply.github.com>
2019-09-17 09:48:28 -0700

    Create task3.py

---
+++ task3.py
@@ -0,0 +1,6 @@
+course = {'python':1500, 'java':1000, 'c':800, 'c++':1200}
+sorted_d = sorted((value,key) for(key,value) in course.items())
+course={}
+for i in sorted_d:
+  course[i[1]] = i[0]
+print(course)
